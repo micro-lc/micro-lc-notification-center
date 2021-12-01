9d725297bf590156ec27b0a10f2b6022c5141928
Umberto Toniolo <amountainram@gmail.com>
2021-12-01 10:48:50 +0100

    webcomponent test refactor

--- src/components/nofication-center/micro-lc-notification-center.spec.tsx
+++ src/components/nofication-center/micro-lc-notification-center.spec.tsx
@@ -1,234 +1,233 @@
-import React from 'react'
-
-import {newSpecPage} from '@stencil/core/testing'
+import {h} from '@stencil/core'
+import {newSpecPage, SpecPage} from '@stencil/core/testing'
 import nock from 'nock'
-import ReactDOM from 'react-dom'
 
+import {JSX} from '../../components'
+import {Notification} from '../../lib'
 import {DEFAULT_PAGINATION_LIMIT} from '../../utils/notificationsClient'
-import {mockNotifications, waitForChanges} from '../../utils/testUtils'
+import Sandbox, {mockNotifications, waitForChanges} from '../../utils/testUtils'
 import {MicroLcNotificationCenter} from './micro-lc-notification-center'
 
 const DEFAULT_NOCK_ENDPOINT = 'http://localhost'
 const NOTIFICATIONS = '/notifications'
+const endpoint = `${DEFAULT_NOCK_ENDPOINT}${NOTIFICATIONS}`
+const mocks = {react: ['createElement'], 'react-dom': ['render', 'unmountComponentAtNode']}
 
 /**
  * initializes a nock get mock to the first 
- * batch of paginated notifications
- * @param limit pagination limit to DEFAULT_PAGINATION_LIMIT = 10
+ * batch of paginated notifications if status is 'ok'
+ * otherwise gives a 500 if 'nok'
+ * @param status {'ok' | 'nok'} http response status
  * @param skip pagination skip defualting to 0
+ * @param limit pagination limit to DEFAULT_PAGINATION_LIMIT (= 10)
  * @returns nock scope
  */
-function init (limit?: number, skip = 0) {
-  const l = limit ?? DEFAULT_PAGINATION_LIMIT
-  const notifications = mockNotifications(l)
-  nock(DEFAULT_NOCK_ENDPOINT)
+function init (status: 'ok' | 'nok', skip = 0, limit = DEFAULT_PAGINATION_LIMIT): Notification[] | undefined {
+  const scope = nock(DEFAULT_NOCK_ENDPOINT)
     .get(NOTIFICATIONS)
-    .query({skip, limit: l})
-    .reply(200, notifications)
-  return notifications
+    .query({skip, limit})
+  if(status === 'ok') {
+    const notifications = mockNotifications(limit)
+    scope.reply(200, notifications)
+    return notifications
+  }
+
+  scope.reply(500)
 }
 
 /**
- * initializes a nock get mock that returns 500
- * @param limit pagination limit to DEFAULT_PAGINATION_LIMIT = 10
- * @param skip pagination skip defualting to 0
+ * Given a jest mock, it returns the `index`-th call
+ * @param mock the jest mock
+ * @param index the nth call index, if not provided it defaults to the last call
+ * @returns the nth jest mock call
  */
-function init500 (limit?: number, skip = 0) {
-  const l = limit ?? DEFAULT_PAGINATION_LIMIT
-  nock(DEFAULT_NOCK_ENDPOINT)
-    .get(NOTIFICATIONS)
-    .query({skip, limit: l})
-    .reply(500)
+function call<T = any, A extends any[] = any> ({mock: {calls}}: jest.Mock<T, A>, index?: number): A {
+  const len = calls.length
+  return calls[index ?? len - 1]
+}
+
+/**
+ * utility function to extract the `micro-lc-notification-center`
+ * webcomponent and its parent
+ * @param page 
+ * @returns an object containing the `notificationCenter` and the `host`
+ */
+function _get (page: SpecPage): {notificationCenter: HTMLMicroLcNotificationCenterElement; host: HTMLElement} {
+  const notificationCenter = page.body.querySelector('micro-lc-notification-center')
+  return {notificationCenter, host: notificationCenter.parentElement}
+}
+
+/**
+ * utility function to create a new `spec-page` with a `micro-lc-notification-center`
+ * embedded
+ * @param props props to attach to `micro-lc-notification-center`
+ * @returns a new `spec-page` promise
+ */
+function create (props: JSX.MicroLcNotificationCenter = {}): Promise<SpecPage> {
+  return newSpecPage({
+    components: [MicroLcNotificationCenter],
+    template: () => h('micro-lc-notification-center', props)//`<micro-lc-notification-center ${endpoint ?? ''}></micro-lc-notification-center>`
+  })
 }
 
 nock.disableNetConnect()
 
 describe('micro-lc-notification-center lifecycle tests', () => {
-  let createElement: any
-  let render: any
-  let unmountComponentAtNode: any
-  beforeAll(() => {
-    createElement = jest.requireActual('react').createElement
-    jest.requireActual('react').createElement = jest.fn()
+  const sandbox = new Sandbox(mocks)
+  const sandboxMocks = sandbox.mock()
 
-    render = jest.requireActual('react-dom').render
-    unmountComponentAtNode = jest.requireActual('react-dom').unmountComponentAtNode
-    jest.requireActual('react-dom').render = jest.fn()
-    jest.requireActual('react-dom').unmountComponentAtNode = jest.fn()
-  })
   afterAll(() => {
-    jest.requireActual('react').createElement = createElement
-    jest.requireActual('react-dom').render = render
-    jest.requireActual('react-dom').unmountComponentAtNode = unmountComponentAtNode
+    sandbox.clearSandbox()
+    jest.restoreAllMocks()
   })
+
   afterEach(() => {
     jest.resetAllMocks()
     expect(nock.isDone()).toBe(true)
+    nock.cleanAll()
   })
 
   it('should avoid fetching notifications on absent endopoint', async () => {
-    await newSpecPage({
-      components: [MicroLcNotificationCenter],
-      html: '<micro-lc-notification-center></micro-lc-notification-center>'
-    })
-    // @ts-ignore
-    expect(React.createElement.mock.calls[0][1]).toMatchObject({loading: undefined, notifications: []})
+    const {react: {createElement}} = sandboxMocks
+    await create()
+    expect(call(createElement, 0)[1]).toMatchObject({loading: undefined, notifications: []})
   })
 
   it('should render, then fetch notifications, after that it should be removed and on remount it should\'n fetch again', async () => {
-    const notifications = init()
-    const page = await newSpecPage({
-      components: [MicroLcNotificationCenter],
-      html: `<micro-lc-notification-center endpoint="${DEFAULT_NOCK_ENDPOINT}${NOTIFICATIONS}"></micro-lc-notification-center>`
-    })
+    // init page with successful notifications fetch
+    const {react: {createElement}, 'react-dom': {unmountComponentAtNode, render}} = sandboxMocks
+    const notifications = init('ok')
+    const page = await create({endpoint})
+    expect(call(createElement, 0)[1]).toMatchObject({loading: true, notifications: []})
 
-    // @ts-ignore
-    expect(React.createElement.mock.calls[0][1]).toMatchObject({loading: true, notifications: []})
-
+    // await for fetching and check the response
     await waitForChanges(page, () => {
       expect(nock.isDone()).toBe(true)
     })
+    expect(call(createElement, 1)[1]).toMatchObject({loading: false, notifications})
 
-    // @ts-ignore
-    expect(React.createElement.mock.calls[1][1]).toMatchObject({loading: false, notifications})
+    // attempt DOM removal of the `notification-center`
+    const {notificationCenter, host} = _get(page)
+    notificationCenter.remove()
+    expect(unmountComponentAtNode).toBeCalledTimes(1)
 
-    const notificationsCenter = page.body.querySelector('micro-lc-notification-center')
-    const host = notificationsCenter.parentElement
-    notificationsCenter.remove()
-    expect(ReactDOM.unmountComponentAtNode).toBeCalledTimes(1)
-
-    // re-attaching doesn't trigger another fetch
-    host.appendChild(notificationsCenter)
-    await page.waitForChanges()
-
-    expect(ReactDOM.render).toBeCalledTimes(3)
+    // re-attaching and chech that there is no extra notifications fetch
+    host.appendChild(notificationCenter)
+    await waitForChanges(page, () => {
+      expect(render).toBeCalledTimes(3)
+    })
   })
 
   it('should render, and fail fatching while transmitting error status', async () => {
-    init500()
-    const page = await newSpecPage({
-      components: [MicroLcNotificationCenter],
-      html: `<micro-lc-notification-center endpoint="${DEFAULT_NOCK_ENDPOINT}${NOTIFICATIONS}"></micro-lc-notification-center>`
-    })
+    // init page with unsuccessful notifications fetch
+    init('nok')
+    const {react: {createElement}} = sandboxMocks
+    const page = await create({endpoint})
+    expect(call(createElement, 0)[1]).toMatchObject({loading: true, notifications: []})
 
-    // @ts-ignore
-    expect(React.createElement.mock.calls[0][1]).toMatchObject({loading: true, notifications: []})
-
+    // on failed fetch the `error` flag is true
     await waitForChanges(page, () => {
       expect(nock.isDone()).toBe(true)
     })
-
-    // @ts-ignore
-    expect(React.createElement.mock.calls[1][1]).toMatchObject({loading: false, notifications: [], error: true})
+    expect(call(createElement, 1)[1]).toMatchObject({loading: false, notifications: [], error: true})
   })
 
-  it('should attempt double fetch', async () => {
-    const p1 = init(DEFAULT_PAGINATION_LIMIT)
+  it('should attempt double fetch, also setting a non-default limit', async () => {
+    // init page with successful notifications fetch
+    const limit = 5
+    const {react: {createElement}} = sandboxMocks
+    const p1 = init('ok', 0, limit)
+    const page = await create({endpoint, limit})
+    const {next} = call(createElement, 0)[1]
 
-    const page = await newSpecPage({
-      components: [MicroLcNotificationCenter],
-      html: `<micro-lc-notification-center endpoint="${DEFAULT_NOCK_ENDPOINT}${NOTIFICATIONS}"></micro-lc-notification-center>`
-    })
-
-    // @ts-ignore
-    const {next} = React.createElement.mock.calls[0][1]
-
+    // await for first notifications batch
     await waitForChanges(page, () => {
       expect(nock.isDone()).toBe(true)
     })
 
-    const p2 = init(DEFAULT_PAGINATION_LIMIT, DEFAULT_PAGINATION_LIMIT)
+    // click on next to fetch more notifications
+    const p2 = init('ok', limit, limit)
     next()
 
+    // check that all notifications are fetched
     await waitForChanges(page, () => {
       expect(nock.isDone()).toBe(true)
     })
-
-    // @ts-ignore
-    expect(React.createElement.mock.calls[2][1]).toMatchObject({loading: false, notifications: [...p1, ...p2]})
+    expect(call(createElement, 2)[1]).toMatchObject({loading: false, notifications: [...p1, ...p2]})
   })
 
   it('should attempt reload after successful fetch', async () => {
-    init()
+    // init page with successful notifications fetch
+    init('ok')
+    const {react: {createElement}} = sandboxMocks
+    const page = await create({endpoint})
+    const {reload} = call(createElement, 0)[1]
 
-    const page = await newSpecPage({
-      components: [MicroLcNotificationCenter],
-      html: `<micro-lc-notification-center endpoint="${DEFAULT_NOCK_ENDPOINT}${NOTIFICATIONS}"></micro-lc-notification-center>`
-    })
-
-    // @ts-ignore
-    const {reload} = React.createElement.mock.calls[0][1]
-
+    // await for first notifications batch
     await waitForChanges(page, () => {
       expect(nock.isDone()).toBe(true)
     })
 
-    const p1 = init()
+    // click on reload
+    const p1 = init('ok')
     reload()
 
+    // expect that notifications are in the same amount as before (pagination resets)
     await waitForChanges(page, () => {
       expect(nock.isDone()).toBe(true)
     })
-
-    // @ts-ignore
-    expect(React.createElement.mock.calls[2][1]).toMatchObject({loading: false, notifications: p1})
+    expect(call(createElement, 2)[1]).toMatchObject({loading: false, notifications: p1})
   })
 
   it('should attempt reload after failed fetch', async () => {
-    init500()
+    // init page with unsuccessful notifications fetch
+    init('nok')
+    const {react: {createElement}} = sandboxMocks
+    const page = await create({endpoint})
+    const {reload} = call(createElement, 0)[1]
 
-    const page = await newSpecPage({
-      components: [MicroLcNotificationCenter],
-      html: `<micro-lc-notification-center endpoint="${DEFAULT_NOCK_ENDPOINT}${NOTIFICATIONS}"></micro-lc-notification-center>`
-    })
-
-    // @ts-ignore
-    const {reload} = React.createElement.mock.calls[0][1]
-
+    // wait for http request failure
     await waitForChanges(page, () => {
       expect(nock.isDone()).toBe(true)
     })
 
-    const p1 = init()
+    // attempt reload
+    const p1 = init('ok')
     reload()
 
+    // check that `error` is false and there's data
     await waitForChanges(page, () => {
       expect(nock.isDone()).toBe(true)
     })
-
-    // @ts-ignore
-    expect(React.createElement.mock.calls[2][1]).toMatchObject({loading: false, notifications: p1})
+    expect(call(createElement, 2)[1]).toMatchObject({loading: false, notifications: p1, error: false})
   })
 
   it('should attempt reload after 2 pages', async () => {
-    init(DEFAULT_PAGINATION_LIMIT)
+    // init page with successful notifications fetch
+    const limit = 21
+    init('ok', 0, limit)
+    const {react: {createElement}} = sandboxMocks
+    const page = await create({endpoint, limit})
+    const {next, reload} = call(createElement, 0)[1]
 
-    const page = await newSpecPage({
-      components: [MicroLcNotificationCenter],
-      html: `<micro-lc-notification-center endpoint="${DEFAULT_NOCK_ENDPOINT}${NOTIFICATIONS}"></micro-lc-notification-center>`
-    })
-
-    // @ts-ignore
-    const {next, reload} = React.createElement.mock.calls[0][1]
-
+    // wait for first notifications batch
     await waitForChanges(page, () => {
       expect(nock.isDone()).toBe(true)
     })
 
-    init(DEFAULT_PAGINATION_LIMIT, DEFAULT_PAGINATION_LIMIT)
+    // click next and await fetching
+    init('ok', limit, limit)
     next()
-
     await waitForChanges(page, () => {
       expect(nock.isDone()).toBe(true)
     })
 
-    const p1 = init(DEFAULT_PAGINATION_LIMIT)
+    // click reload and check pagination reset
+    const p1 = init('ok', 0, limit)
     reload()
     await waitForChanges(page, () => {
       expect(nock.isDone()).toBe(true)
     })
-
-    // @ts-ignore
-    expect(React.createElement.mock.calls[3][1]).toMatchObject({loading: false, notifications: p1})
+    expect(call(createElement, 3)[1]).toMatchObject({loading: false, notifications: p1})
   })
 })
--- src/utils/testUtils.ts
+++ src/utils/testUtils.ts
@@ -64,4 +64,67 @@ async function waitForChanges<T = any> (page: SpecPage, callback?: () => T, opts
     .then(() => page.waitForChanges())
 }
 
-export {mockNotifications, waitForChanges}
+type MethodsToMockByModule = Record<string, string[]>
+
+export type MocksMap = Record<string, Record<string, jest.Mock>>
+
+export default class Sandbox {
+  private mocksMap: MethodsToMockByModule = {}
+  private actuals: MocksMap = {}
+
+  constructor (mocksMap: MethodsToMockByModule) {
+    this.mocksMap = mocksMap
+    Object.entries(mocksMap).reduce(
+      (acc, [module, methods]) => {
+        methods.forEach(method => {
+          let actual: any
+          try {
+            actual = jest.requireActual(module)[method]
+            if (acc[module]) {
+              acc[module][method] = actual
+            } else {
+              acc[module] = {[method]: actual}
+            }
+          } catch (err) {
+            throw new Error(`cannot require method: ${method} on module: ${module}. Sandbox constructor threw: ${err}`)
+          }
+        })
+        return acc
+      }, this.actuals as MocksMap)
+  }
+
+  mock (): MocksMap {
+    return Object.entries(this.mocksMap).reduce((mocks, [module, methods]) => {
+      methods.forEach(method => {
+        try {
+          const mock = jest.fn()
+          jest.requireActual(module)[method] = mock
+          if (mocks[module]) {
+            mocks[module][method] = mock
+          } else {
+            mocks[module] = {[method]: mock}
+          }
+        } catch (err) {
+          throw new Error(`cannot require method: ${method} on module: ${module}. mock threw: ${err}`)
+        }
+      })
+      return mocks
+    }, {} as MocksMap)
+  }
+
+  clearSandbox (): void {
+    Object.entries(this.mocksMap).forEach(([module, methods]) => {
+      methods.forEach(method => {
+        try {
+          jest.requireActual(module)[method] = this.actuals[module]?.[method]
+        } catch (err) {
+          throw new Error(`cannot require method: ${method} on module: ${module}. clearSandbox threw: ${err}`)
+        }
+      })
+    })
+    this.actuals = {}
+  }
+}
+
+
+export {mockNotifications, waitForChanges, Sandbox}
